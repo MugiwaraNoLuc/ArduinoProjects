380ba49a84e2f61ec2c85b66177de9c8ba463e82
franc <franco100lucas@gmail.com>
2023-03-03 21:36:51 -0300

    atualização incremento

--- 9-botão_incremento.ino
+++ 9-botão_incremento.ino
@@ -3,7 +3,6 @@
 int ValorBotao;
 
 int incremento = 0;
-
  
 void setup(){
     Serial.begin(9600);
@@ -29,3 +28,46 @@ void loop(){
     }
   }
 }
+
+
+/*
+"""
+Atualização do código, fazendo o incremento do botão por meio de função
+"""
+#define botao 2
+#define botao2 4
+
+int ValorBotao;
+int ValorBotao2;
+
+int incremento = 0;
+int incremento2 = 0;
+
+void botao_leitura(){
+      if(ValorBotao == HIGH){
+    delay(200);
+    
+    if(0<=incremento){
+    Serial.print("Leitura: ");
+    Serial.println(incremento);
+    delay(300);
+    incremento++;
+      
+      if(incremento>9){
+        incremento = 0;
+      }
+    }
+  }
+}
+ 
+void setup(){
+    Serial.begin(9600);
+    pinMode(botao, INPUT);
+}
+
+void loop(){
+    ValorBotao = digitalRead(botao);
+    ValorBotao2 = digitalRead(botao2);
+  botao_leitura();
+}
+*/
